0324e3547d0dbc87243c47f6e1b8e8494e893604
a4Boy <85559104+ardyop@users.noreply.github.com>
2022-12-03 14:24:55 +0000

    Adding program detail

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
+# Components
 import hashlib
 import pandas
-print("Hello world", 7)
-print(5)
-print("bye")
-print(17*13)
+
+# Variables
+a = complex(8, 2)
+b = True
+c = "Ardy"
+d = None
+
+#program
+print(a)
+a1 = 9
+print(b)
+print(a + a1)
+print("the type of a is ", type(a))
+print("the type of b is ", type(b))
+print("the type of c is ", type(c))
+
+list1 =[8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list1)
+
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+
+dict1 = {"name":"Sakshi", "age":20, "canVote":True}
+print(dict1)
